c6a24f839934612fdb26eec0e69088f0a244a28b
Truffel <tomsonrz@protonmail.ch>
2023-01-09 11:53:39 +0100

    v0.3.4b

--- src/accounts/management/commands/root.py
+++ src/accounts/management/commands/root.py
@@ -8,9 +8,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # Create a root user
         try:
-            user = User.objects.create(username='root',password='root',email='root@root.root',
+            user = User.objects.get_or_create(username='root',email='root@root.root',
                                        is_superuser=True, is_staff=True, is_active=True, is_email_verified=True)
+            user.set_password('root')
             user.save()
+
         except Exception as e:
             print(e)
             print('Root user already exists')
