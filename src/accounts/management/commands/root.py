71515f80938b881b117369c96a8b6548c1e959ff
Truffel <tomsonrz@protonmail.ch>
2023-01-09 11:55:08 +0100

    v0.3.4c

--- src/accounts/management/commands/root.py
+++ src/accounts/management/commands/root.py
@@ -8,10 +8,15 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # Create a root user
         try:
-            user = User.objects.get_or_create(username='root',email='root@root.root',
+            try:
+                user = User.objects.create(username='root',email='root@root.root',
                                        is_superuser=True, is_staff=True, is_active=True, is_email_verified=True)
-            user.set_password('root')
-            user.save()
+                user.set_password('root')
+                user.save()
+            except:
+                user = User.objects.filter(username='root').first()
+                user.set_password('root')
+                user.save()
 
         except Exception as e:
             print(e)
